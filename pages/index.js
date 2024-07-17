f3777286542de1150b3a52c1072901fe5af16362
Glen Adamson <glenadamson1984@gmail.com>
2024-07-17 13:25:15 +0100

    fixing ui on stock - using a simple table and static data

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,37 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <>
       <h2 className="mb-4 text-xl font-semibold">Current Stock</h2>
+      <table className="min-w-full bg-white">
+        <thead>
+          <tr>
+            <th className="px-6 py-3 text-left border-b-2 border-gray-300">
+              Name
+            </th>
+            <th className="px-6 py-3 text-left border-b-2 border-gray-300">
+              Unit
+            </th>
+            <th className="px-6 py-3 text-left border-b-2 border-gray-300">
+              Cost
+            </th>
+            <th className="px-6 py-3 text-left border-b-2 border-gray-300">
+              Current Quantity
+            </th>
+          </tr>
+        </thead>
 
-      {ingredients.map((ingredient) => (
-        <div key={ingredient.id} className="flex">
-          <div className="flex flex-row">
-            {ingredient.name}
-            <div>{ingredient.unit}</div>
-            <div>{ingredient.cost}</div>
-            <div>{ingredient.current_quantity}</div>
-          </div>
-        </div>
-      ))}
-    </div>
+        <tbody>
+          {ingredients.map((ingredient) => (
+            <tr key={ingredient.ingredient_id}>
+              <td className="px-6 text-left"> {ingredient.name}</td>
+              <td className="px-6 text-left">{ingredient.unit}</td>
+              <td className="px-6 text-left">{ingredient.cost}</td>
+              <td className="px-6 text-left">{ingredient.current_quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
